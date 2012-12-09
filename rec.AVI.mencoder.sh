e8b30e1e2917e141f53b8c177b083ba21c4753fa
sebkst <seb@k.st>
2012-12-09 14:56:44 +0100

    added : a movie player in Gambas3 depending on mplayer

--- rec.AVI.mencoder.sh
+++ rec.AVI.mencoder.sh
@@ -1,7 +1,8 @@
 
 # apt-get install liblzo2-2 mencoder
 
-[ "$1" = "-h" ] && ( echo "usage: $0  DEVICE  WIDTH  HEIGHT OUTPUT.avi" )
+[ "$1" = "-h" ] && ( echo "usage: $0  DEVICE  WIDTH  HEIGHT OUTPUT.avi" ; echo "outputs a RIFF (little-endian) data, AVI, W x H, 30.00 fps, video: FFMpeg MPEG-4" )
+
 
 [ "$#" -gt 3 ] && mencoder tv:// -tv driver=v4l2:width=$2:height=$3:device=$1 -nosound -ovc lavc -o $4
 
